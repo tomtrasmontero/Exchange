2401204d518ac607c7864c919498f076004d746c
tomtrasmontero <tomtrasmontero@gmail.com>
2021-10-11 19:44:33 -0400

    Update client and server with signature verification

--- client/index.js
+++ client/index.js
@@ -1,5 +1,8 @@
+import SHA256 from "crypto-js/sha256";
 import "./index.scss";
 
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 const server = "http://localhost:3042";
 
 document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
@@ -19,12 +22,18 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const privateKey = document.getElementById("private-key").value;
+  const key = ec.keyFromPrivate(privateKey, 'hex');
 
-  const body = JSON.stringify({
+  const body = {
     sender, amount, recipient
-  });
+  };
 
-  const request = new Request(`${server}/send`, { method: 'POST', body });
+  const payload = SHA256(JSON.stringify(body)).toString();
+  const signature = key.sign(payload);
+  const signedBody = JSON.stringify({...body, signedPayload: payload, signature: signature.toDER('hex'), publicKey: key.getPublic().encode('hex')});
+  console.log(signedBody);
+  const request = new Request(`${server}/send`, { method: 'POST', signedBody });
 
   fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
     return response.json();
--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const SHA256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
 const app = express();
 const cors = require('cors');
 const port = 3042;
+const ec = new EC('secp256k1');
 
 // localhost can have cross origin errors
 // depending on the browser you use!
@@ -14,19 +17,65 @@ const balances = {
   "3": 75,
 }
 
+const privateKeyMap = {};
+const publicBalances = {};
+
+function createPrivateKeys(accounts) {
+  const accountsKeyList = Object.keys(accounts);
+  accountsKeyList.forEach(account => {
+    const privateKey = ec.genKeyPair();
+    const publicKey = privateKey.getPublic().encode('hex');
+    const accountBal = balances[account];
+    privateKeyMap[publicKey] = privateKey;
+    publicBalances[publicKey] = accountBal;
+  });
+}
+
+function showPubBalanceAndKey(pubAccounts) {
+  let accountsList = "";
+  let privateKeyList = "";
+  const accountsKeyList = Object.keys(pubAccounts);
+  accountsKeyList.map((account, idx) => {
+    const accountBal = pubAccounts[account];
+    const privateKey = privateKeyMap[account];
+    accountsList = accountsList + `(${idx}) ${account} (${accountBal} ETH) \n`;
+    privateKeyList = privateKeyList + `(${idx}) ${privateKey.getPrivate().toString(16)} \n`;
+  })
+
+  console.log(`
+  Available Accounts
+  ==================\n${accountsList}
+  Private Keys
+  ==================\n${privateKeyList}
+  `)
+}
+
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
-  const balance = balances[address] || 0;
+  const balance = publicBalances[address] || 0;
   res.send({ balance });
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  const {sender, recipient, amount, signature, publicKey, signedPayload} = req.body;
+  console.log(req.body);
+  console.log(req.body.signature, req.body.signedPayload);
+  const key = ec.keyFromPublic(publicKey, 'hex');
+  const hashedBody = SHA256(JSON.stringify(signedPayload)).toString();
+  console.log(signature, signedPayload);
+  const isSignatureVerified = key.verify(hashedBody, signature);
+
+  if (isSignatureVerified) {
+    balances[sender] -= amount;
+    balances[recipient] = (balances[recipient] || 0) + +amount;
+    res.send({ balance: balances[sender] });
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
+  createPrivateKeys(balances)
+  showPubBalanceAndKey(publicBalances);
 });
